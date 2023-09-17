2b8e70825755ffbe0449bfeb608ecd1412227e7c
name <you@example.com>
2023-09-16 19:38:15 -0500

    _

---
+++ client/src/app/api/terceros/controller.ts
@@ -0,0 +1,25 @@
+import type { Tercero } from '@/types'
+import { axios } from '../axios'
+
+const PAGE = '/terceros/'
+const BASE_URL = 'http://localhost:3000' + PAGE
+
+export async function getAll (id?: string): Promise<Tercero[]> {
+  return await axios<Tercero[]>(BASE_URL)
+}
+
+export async function create (tercero: Tercero): Promise<Tercero> {
+  return await axios<Tercero>(BASE_URL, 'POST', tercero)
+}
+
+export async function getById (id: string): Promise<Tercero> {
+  return await axios<Tercero>(BASE_URL + id)
+}
+
+export async function deleteById (id: string): Promise<Tercero> {
+  return await axios<Tercero>(BASE_URL + id, 'DELETE')
+}
+
+export async function update (id: string, tercero: Tercero): Promise<Tercero> {
+  return await axios<Tercero>(BASE_URL + id, 'PATCH', tercero)
+}

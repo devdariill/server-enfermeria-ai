aa7398aa9e4f3dcc61e85d576952fc3632fa027e
name <you@example.com>
2023-09-27 22:19:02 -0500

    planificacion

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,12 +4,9 @@ import { ButtonLink } from '../components/button-link'
 export default async function Home () {
   return (
     <main className='mx-auto px-4 gap-5 grid py-10'>
-      <ButtonLink href='/ai'>
-        Historias Clinicas
-      </ButtonLink>
-      <ButtonLink href='/enfermeria/tercero'>
-        Terceros
-      </ButtonLink>
+      <ButtonLink href='/ai'>Historias Clinicas</ButtonLink>
+      <ButtonLink href='/enfermeria/tercero'>Terceros</ButtonLink>
+      <ButtonLink href='/enfermeria/planificacion'>Planificaciones</ButtonLink>
 
       <Logout />
 

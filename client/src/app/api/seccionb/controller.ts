e18f69ff876cd6df447cc0a3ffa2a141c5d8c261
name <gowluonade@gmail.com>
2023-10-10 13:33:28 -0500

    _url_deploy

--- client/src/app/api/seccionb/controller.ts
+++ client/src/app/api/seccionb/controller.ts
@@ -2,7 +2,9 @@ import type { SeccionB } from '@/types'
 import { axios } from '../axios'
 
 const PAGE = '/seccionb'
-const BASE_URL = 'http://localhost:3001' + PAGE
+const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.URL_BACK! + PAGE : 'http://localhost:3001' + PAGE
+// const BASE_URL = 'http://localhost:3001' + PAGE
+// const URL_BACK = process.env.NODE_ENV === 'production' ? process.env.URL_BACK! + '/informes' : 'http://localhost:3001/informes'
 
 export async function getAll (id: string): Promise<SeccionB[]> {
   return await axios<SeccionB[]>(BASE_URL + '?id=' + id)
